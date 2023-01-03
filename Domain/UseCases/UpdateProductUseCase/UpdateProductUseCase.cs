371598f3c93ea879b71878a9b1c0db081fa7fde6
Juan Daniel <juandanielarce398@gmail.com>
2023-01-03 18:14:20 -0500

    Update Product use case created

--- Domain/UseCases/UpdateProductUseCase/UpdateProductUseCase.cs
+++ Domain/UseCases/UpdateProductUseCase/UpdateProductUseCase.cs
@@ -1,6 +1,19 @@
+using Domain.Models;
+using Domain.UseCases.CreateProductUseCase.Ports;
+
 namespace Domain.UseCases.UpdateProductUseCase;
 
-public class UpdateProductUseCase
+public class UpdateProductUseCase : UseCase<Task<Product>, Product>
 {
-    
+    private readonly IProductRepository _productRepository;
+
+    public UpdateProductUseCase(IProductRepository productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public override async Task<Product> Apply(Product product)
+    {
+        return await _productRepository.UpdateAsync(product);
+    }
 }

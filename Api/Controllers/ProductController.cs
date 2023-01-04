dd5df3acc5c19698ff85c548e3f26df7f0252f08
Juan Daniel <juandanielarce398@gmail.com>
2023-01-04 08:36:14 -0500

    product crud complete

--- Api/Controllers/ProductController.cs
+++ Api/Controllers/ProductController.cs
@@ -2,6 +2,10 @@ using Api.Dtos;
 using AutoMapper;
 using Domain.Models;
 using Domain.UseCases;
+using Domain.UseCases.CreateProductUseCase;
+using Domain.UseCases.DeleteProductUseCase;
+using Domain.UseCases.GetProductUseCase;
+using Domain.UseCases.UpdateProductUseCase;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers;
@@ -19,12 +23,53 @@ public class ProductController : ControllerBase
 
     [HttpPost]
     public async Task<ActionResult> CreateProductAsync([FromBody] CreateProductDto createProductDto,
-        [FromServices] UseCase<Task<Product>, Product> useCase)
+        [FromServices] IEnumerable<UseCase<Task<Product>, Product>> createProductUseCase)
     {
+        var useCase = createProductUseCase.Single(_ => _.GetType() == typeof(CreateProductUseCase));
         var product = _mapper.Map<Product>(createProductDto);
 
         await useCase.Apply(product);
-        // return CreatedAtAction()
-        return Ok();
+        return CreatedAtAction(nameof(GetProductAsync), new { id = product.Id }, product);
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult> GetProductAsync([FromRoute] int id,
+        [FromServices] IEnumerable<UseCase<Task<Product>, Product>> getProductUseCase)
+    {
+        var useCase = getProductUseCase.Single(_ => _.GetType() == typeof(GetProductUseCase));
+        var result = await useCase.Apply(new Product { Id = id });
+
+        return Ok(result);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> GetAllProductsAsync(
+        [FromServices] UseCase<Task<List<Product>>, Product> useCase)
+    {
+        var result = await useCase.Apply(new Product());
+
+        return Ok(result.Select(product => _mapper.Map<ProductDto>(product)));
+    }
+
+    //TODO: Update
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult> UpdateProductAsync([FromRoute] int id, [FromBody] CreateProductDto createProductDto,
+        [FromServices] IEnumerable<UseCase<Task<Product>, Product>> updateProductUseCase)
+    {
+        var useCase = updateProductUseCase.Single(_ => _.GetType() == typeof(UpdateProductUseCase));
+        var product = _mapper.Map<Product>(createProductDto);
+        var result = await useCase.Apply(product with { Id = id });
+
+        return Ok(_mapper.Map<ProductDto>(result));
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult> DeleteProduct(int id,
+        [FromServices] IEnumerable<UseCase<Task<Product>, Product>> deleteProductUseCase)
+    {
+        var useCase = deleteProductUseCase.Single(_ => _.GetType() == typeof(DeleteProductUseCase));
+
+        var deletedProduct = await useCase.Apply(new Product { Id = id });
+        return Ok(deletedProduct);
     }
 }
--- Api/Startup.cs
+++ Api/Startup.cs
@@ -3,6 +3,9 @@ using Domain.Models;
 using Domain.UseCases;
 using Domain.UseCases.CreateProductUseCase;
 using Domain.UseCases.CreateProductUseCase.Ports;
+using Domain.UseCases.DeleteProductUseCase;
+using Domain.UseCases.GetProductUseCase;
+using Domain.UseCases.UpdateProductUseCase;
 using Infrastructure;
 using Infrastructure.Adapters;
 using Microsoft.EntityFrameworkCore;
@@ -26,12 +29,19 @@ public class Startup
         services.AddSwaggerGen();
 
         services.AddScoped<UseCase<Task<Product>, Product>, CreateProductUseCase>();
+        services.AddScoped<UseCase<Task<Product>, Product>, UpdateProductUseCase>();
+        services.AddScoped<UseCase<Task<Product>, Product>, DeleteProductUseCase>();
+        services.AddScoped<UseCase<Task<Product>, Product>, GetProductUseCase>();
+        services.AddScoped<UseCase<Task<List<Product>>, Product>, GetAllProductsUseCase>();
+
         services.AddScoped<IProductRepository, ProductAdapter>();
 
         services.AddDbContext<Context>(options => options.UseSqlServer(Configuration.GetConnectionString("SqlServer"),
             builder => builder.MigrationsAssembly("Infrastructure")));
 
         services.AddAutoMapper(typeof(Startup));
+        
+        services.AddControllers(options => { options.SuppressAsyncSuffixInActionNames = false; }); 
     }
 
     public void Configure(WebApplication app, IWebHostEnvironment env)

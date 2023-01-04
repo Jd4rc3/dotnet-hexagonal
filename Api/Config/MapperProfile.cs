25ee163054189e4992945a0e5120f24ac5d8c551
Juan Daniel <juandanielarce398@gmail.com>
2023-01-04 13:48:07 -0500

    Mapper profile config to buydtos

--- Api/Config/MapperProfile.cs
+++ Api/Config/MapperProfile.cs
@@ -6,9 +6,23 @@ namespace Api.Config;
 
 public class MapperProfile : Profile
 {
+    public Func<PurchaseDto, Buy, IEnumerable<ProductBuy>> Map = (productBuyDtos, buy) =>
+    {
+        return productBuyDtos.Buys.Select(productBuyDto => new ProductBuy
+        {
+            Product = new Product { Id = productBuyDto.ProductId },
+            Quantity = productBuyDto.Quantity
+        });
+    };
+
     public MapperProfile()
     {
         CreateMap<CreateProductDto, Product>().ReverseMap();
         CreateMap<ProductDto, Product>().ReverseMap();
+        CreateMap<CreateProductBuyDto, ProductBuy>().ReverseMap();
+        CreateMap<PurchaseDto, Buy>().ReverseMap();
+
+        CreateMap<PurchaseDto, Buy>().ForMember(entity => entity.Buys,
+            opt => opt.MapFrom(Map));
     }
 }
